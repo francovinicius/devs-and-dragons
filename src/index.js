01aa272bd45321df84c0c86466120298dbc7cacf
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-04-12 12:01:38 -0300

    add functions for: create character + add atributs + view character

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
-}
+import { Personagem } from "./modules/personagem.js"
+import { personagemView } from "./components/personagem-view.js"
+import { Mago } from "./modules/mago.js"
+import { Arqueiro } from "./modules/arqueiro.js"
+import { ArqueiroMago } from "./modules/arqueiro-mago.js"
 
-const personagemJose = {
-    nome: 'Jose',
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
-}
+const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3)
+const magaJulia = new Mago('Julia', 7, 'gelo', 7, 13)
+const magoWagner = new Mago('Wagner', 7, 'luz', 6, 4)
 
+
+const arqueiroWagner = new Arqueiro('Wagner', 7, 10)
+
+const arqueiroMagoVini = new ArqueiroMago('Vinicius', 51, 10, 'ar', 5, 9)
+
+const personagens = [magaJulia, magoAntonio, magoWagner, arqueiroWagner, arqueiroMagoVini ]
+
+new personagemView(personagens).render()
+
+
+console.log(Personagem.verificarVencedor(arqueiroMagoVini, magoAntonio))
